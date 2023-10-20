d758d4f5db2cedf8fa8000c9abc2ddc6af681ab5
ARTURO MEDINA JIMÉNEZ <arturo.medina@exceltic.com>
2023-10-20 08:30:12 +0200

    Filename change

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uuid
 import os
 from PIL import ImageGrab, Image
 
@@ -5,4 +6,5 @@ clipboard_content = ImageGrab.grabclipboard()
 
 if isinstance(clipboard_content, Image.Image):
     desktop_dir = os.path.join(os.path.expanduser('~'), 'Desktop')
-    clipboard_content.save(os.path.join(desktop_dir, 'clipboard_image.png'))
+    unique_filename = str(uuid.uuid4())
+    clipboard_content.save(os.path.join(desktop_dir, f'img-{unique_filename}.png'))
